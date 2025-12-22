15c5f7166973841f4b0f4edb28f47e84b86c782a
conao3 <conao3@gmail.com>
2025-12-22 23:32:05 +0900

    add simple validator

--- src/clantic/core.clj
+++ src/clantic/core.clj
@@ -1,7 +1,20 @@
 (ns clantic.core
   (:require
-   [malli.core :as m]))
+   [malli.core :as m]
+   [malli.error :as me]))
 
-(defn hello [name]
-  (str "Hello, " name "!"))
-(m/=> hello [:=> [:cat :int] :string])
+(defn- schema->malli [schema]
+  (into [:map] (map (fn [[k v]] [k v]) schema)))
+
+(defn- explain-humanized [schema value]
+  (some-> (m/explain schema value) me/humanize))
+
+(defn model-validate
+  [schema value]
+  (let [malli-schema (schema->malli schema)]
+    (if (m/validate malli-schema value)
+      value
+      (throw (ex-info "Validation failed" {:errors (explain-humanized malli-schema value)
+                                           :value value
+                                           :schema schema})))))
+(m/=> model-validate [:=> [:cat :map :map] :map])
--- test/clantic/core_test.clj
+++ test/clantic/core_test.clj
@@ -1,7 +1,29 @@
 (ns clantic.core-test
   (:require
-   [clojure.test :refer [deftest is]]
-   [clantic.core :as sut]))
+   [clojure.test :as t]
+   [clantic.core :as c]))
 
-(deftest hello-test
-  (is (= "Hello, world!" (sut/hello "world"))))
+(t/deftest model-validate-test
+  (t/testing "returns value for valid data"
+    (t/is (= {:name "Alice" :age 30}
+             (c/model-validate {:name :string :age :int}
+                               {:name "Alice" :age 30}))))
+
+  (t/testing "throws for invalid data"
+    (t/is (thrown-with-msg? clojure.lang.ExceptionInfo #"Validation failed"
+            (c/model-validate {:name :string :age :int}
+                              {:name "Alice" :age "30"})))
+    (let [ex (try
+               (c/model-validate {:name :string :age :int}
+                                 {:name 123 :age "30"})
+               (catch Exception e e))]
+      (t/is (= {:name ["should be a string"] :age ["should be an integer"]}
+               (:errors (ex-data ex))))))
+
+  (t/testing "missing keys"
+    (let [ex (try
+               (c/model-validate {:name :string :age :int}
+                                 {:name "Alice"})
+               (catch Exception e e))]
+      (t/is (= {:age ["missing required key"]}
+               (:errors (ex-data ex)))))))
